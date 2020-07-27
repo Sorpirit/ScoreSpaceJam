703492064c9b40648a2359ced94eb89462a49062
Sorp <danvuk11@gmail.com>
2020-07-27 20:55:10 +0300

    add score

---
+++ WallRunner/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private float lifes;
+
+    public Action OnPlayerDead;
+
+    public void Damage()
+    {
+        lifes--;
+        if(lifes <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
