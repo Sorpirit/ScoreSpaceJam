0d1edfbee2bf21ef1e9bd6ba68735d61eb60c6a3
nikinov <novellenicholas@gmail.com>
2020-07-27 19:32:04 +0200

    Revert "..."
    This reverts commit ff8b7f15377526c1078794a275f292019ef73327.

--- WallRunner/Assets/Scripts/other/GameManager.cs
+++ WallRunner/Assets/Scripts/other/GameManager.cs
@@ -6,13 +6,10 @@ public class GameManager : MonoBehaviour
 {
     public GameObject Enviroment;
     private GameObject go;
-    public CanvasGroup blackPanel;
-    public CanvasGroup MainPanel;
-    public GameObject Player;
 
     private void Update()
     {
-        if (go.transform.position.y <= 21)
+        if (go.transform.position.y <= 20)
         {
             spawn();
         }
@@ -24,14 +21,5 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         spawn();
-        LeanTween.alphaCanvas(blackPanel, 0, 1);
-        LeanTween.alphaCanvas(MainPanel, 1, 1f);
     }
-    public void startGame()
-    {
-        LeanTween.alphaCanvas(MainPanel, 0, .5f);
-        MainPanel.gameObject.SetActive(false);
-        Player.transform.position = new Vector3(0, 40f, -5.54f);
-    }
-
 }
